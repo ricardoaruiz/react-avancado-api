b0e4d672baf5e26f890ca289ddc3276de6c65ef0
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-04-05 19:48:43 -0300

    Adicionando comando para gerar dump do postgres

--- tools/restore-dump.sh
+++ tools/restore-dump.sh
@@ -1,2 +1,4 @@
 # cat your_dump.sql | docker exec -i your-db-container psql -U postgres
 cat dump_03-04-2021_19_54_52.sql | docker exec -i react-avancado-api_postgres_1 psql -U strapi
+
+pg_restore --host localhost --port 5432 --username strapi --dbname strapi ./strapi.dump
